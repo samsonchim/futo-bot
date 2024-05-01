9c9f43dcb92809d1ca9b2e10838cb07d8812c371
Samson Chi <fiercesamsonchi@gmail.com>
2024-05-02 00:36:51 +0100

    hughj

--- webhook.js
+++ webhook.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const server = app.listen(process.env.PORT || 5000, () => {
+const server = app.listen(10000, () => {
   console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
 });
 
